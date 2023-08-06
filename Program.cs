703a7a45c595104f82097605c39e9d0206625d80
Yago Bonardi <yagobonardi@MacBook-Pro-de-Yago.local>
2023-08-05 22:49:02 -0300

    listando endpoints da f1 api

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using var client = new HttpClient();
+
+var drivers = await client.GetStringAsync("http://ergast.com/api/f1/2023/drivers");
+var scheduleRacesForSeason = await client.GetStringAsync("http://ergast.com/api/f1/2023");
+var qualifyingResults = await client.GetStringAsync("http://ergast.com/api/f1/2023/1/qualifying");
+var raceResult = await client.GetStringAsync("http://ergast.com/api/f1/2023/1/results"); //with fatest lap
